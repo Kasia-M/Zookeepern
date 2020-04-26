6b05ab678087513c793792152f50c9a7ecbb140a
Kasia-M <kasiamurdzia@gmail.com>
2020-04-26 21:44:40 +0200

    projekt 1

---
+++ Problems/An empty list!/task.py
@@ -0,0 +1,2 @@
+empty_list_1 = []
+print(empty_list_1)
---
+++ Problems/Update a list/task.py
@@ -0,0 +1,7 @@
+numbers = [4, 1, 0, 3, 2, 5]
+elem_1 = numbers[0]
+elem_2 = numbers[1]
+elem_3 = numbers[2]
+elem_4 = numbers[3]
+elem_5 = numbers[4]
+print(len(numbers))
